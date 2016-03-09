9be236e122f80a3c9198fd4170022b0817efcbc5
K.C. Hunter <kchunter@connectionseducation.com>
2016-03-09 18:30:42 -0500

    adding concatenating string example using Collider Movie Talk.

---
+++ concat/index.php
@@ -0,0 +1,10 @@
+<?php
+$showName           = "Collider Movie Talk";
+$host1              = "John Campea";
+$host2              = "Dennis Tzeng";
+$host3              = "Kristian Harloff";
+$host4              = "John Schnepp";
+$host5              = "Mark Ellis";
+
+echo "Welcome to $showName, hosted by: $host1, $host2, $host3, $host4, and $host5.";
+?>
