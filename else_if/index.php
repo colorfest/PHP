3b9830bbe566d2d6737d75b28450169b5e6ad09f
K.C. Hunter <kchunter@connectionseducation.com>
2016-03-09 19:48:59 -0500

    Using else, if, and elseif statements in PHP

---
+++ else_if/index.php
@@ -0,0 +1,17 @@
+<?php
+$name       = "John Scnepp";
+$group      = "Kanjiklub";
+
+if($name == "Mark Ellis")
+{
+    echo "Is a member of $group";
+}
+elseif($name == "John Campea")
+{
+    echo "Author of the book 'The Pride'.";
+}
+else
+{
+    echo "Is a host on Movie Talk.";
+}
+?>
