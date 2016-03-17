696c4dae816392aae300180bba6d1abe3dd180f5
K.C. Hunter <kchunter@connectionseducation.com>
2016-03-17 12:55:42 -0400

    adding operators tutorial sample.

---
+++ operators/index.php
@@ -0,0 +1,30 @@
+<?php
+//$number     = 1;
+//$number     += 2;
+
+//$number .= ' is my number!';
+
+//echo $number;
+
+$n1         = 100;
+$n2         = 300;
+$n3         = 2;
+
+$answer     = ($n1 + $n2) / $n3;
+echo $answer;
+
+$n4         = 11;
+$n5         = 3;
+
+$answer2    = $n4%$n5;
+echo $answer2;
+
+/*if ($n1 >= $n2)
+{
+    echo "$n1 is greater than $n2.";
+}
+else
+{
+    echo "$n1 is not equal to $n2.";
+}*/
+?>
