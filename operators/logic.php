e2665edd3c2f6eb087637348443b3d74c709417c
K.C. Hunter <kchunter@connectionseducation.com>
2016-03-29 11:32:39 -0400

    logical operators

---
+++ operators/logic.php
@@ -0,0 +1,26 @@
+<?php
+$x      = 40;
+$min    = 50;
+$max    = 3000;
+
+if ( ($x>=$min) && ($x<=$max) )
+{
+    echo "Pass.";
+}
+else
+{
+    echo "Fail";
+}
+
+
+/*$n      = "My Name.";
+
+if (!$n)
+{
+    echo "This variable does not exist.";
+}
+else
+{
+    echo "The variable does exist: $n";
+}*/
+?>
